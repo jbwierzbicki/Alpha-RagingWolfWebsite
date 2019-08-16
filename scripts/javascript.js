afba510ad1e7ccb854291e811967334a42f75d9a
Johnathan <jbwierzbicki@gmail.com>
2019-08-16 11:27:13 -0400

    ready to publish page

---
+++ scripts/javascript.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(e){ 
+    var $el = $('.call-us-row'); 
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if ($(this).scrollTop() > 200 && !isPositionFixed){ 
+      $el.css({'position': 'fixed', 'top': '0px', 'left': '15px', 'width': '100%'}); 
+    }
+    if ($(this).scrollTop() < 200 && isPositionFixed){
+      $el.css({'position': 'static', 'top': '0px', 'width': 'auto'}); 
+    } 
+  });
